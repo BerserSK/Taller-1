a60fc5d7e640fb65f2e116c99a7b0b61daf4ad99
Your Name <andres2003latorre@gmail.com>
2022-08-04 12:03:56 -0500

    Ejercicios con ciclos

---
+++ js/script1.js
@@ -0,0 +1,22 @@
+/*let nombre=prompt("Ingrese su nombre");
+let apellido=prompt("Ingrese su apellido");
+window.alert(`Su nombre es ${nombre.toUpperCase()}, la longitud de su nombre es ${nombre.length} y la primera letra es ${nombre.charAt(0)}
+Su apellido es ${apellido.toLowerCase()} y la longitud de su apellido es ${apellido.length}
+Su nombre de usuario es el ${concat (nombre, apellido)}`);
+
+for(i = 0; i<6; i++){
+    console.log("hola mundo")
+}*/
+
+/*let repetir=1;
+
+do{
+    console.log(`Hola soy William`)
+    repetir=confirm("Desea Continuar?")
+}while(repetir=true);
+
+while(repetir=1){
+    window.alert(`Hola soy William`);
+    repetir=prompt("para continuar digita 1 o 2 para salir");
+}
+*/
