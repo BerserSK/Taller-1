e742d5c6eb98658b754a64cd5f4566a3c11ea042
Your Name <andres2003latorre@gmail.com>
2022-08-25 10:35:28 -0500

    Terminada la actividad de profesores

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,4 +1,5 @@
-function calcular(){
+const a=document.getElementById("submit")
+a.addEventListener('click', ()=>{
     let num1=parseInt(document.getElementById("num1").value);
     let num2=parseInt(document.getElementById("num2").value);
     let op=document.getElementById("operacion").value;
@@ -26,4 +27,27 @@ function calcular(){
       alert(`La opcion Elegida no es valida, Por favor vuelva
       a elegir una operacion valida`)
     }
+})  
+const rta=document.getElementById("resultado");
+const ops=document.getElementById("ops");
+ops.addEventListener('click',(e)=>{
+  e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.id);
+  
+if(e.target.id=="btn-sumar"){
+  let texto=`<p>
+    <i class="fa-solid fa-check"></i>
+  </p>`
+  rta.innerHTML=texto; 
+  rta.style.setProperty('transition', ".2s ease all");
+  rta.style.setProperty('color', "red") 
 }
+else {
+  let texto2=`<p>
+  <i class="fa-solid fa-x"></i>
+  </p>`
+  rta.innerHTML=texto2;
+}
+})
+
---
+++ js/profesores.js
@@ -0,0 +1,40 @@
+const listar=document.getElementById("submit")
+const rta=document.getElementById("form")
+listar.addEventListener('click', (e)=>{
+    let op=document.getElementById("nombre").value;
+    let op2=document.getElementById("apellido").value;
+    let op3=document.getElementById("actividad").value;
+    if(op==op2 && op==op3){
+        e.preventDefault();
+        let texto=`
+        <i class="fa-solid fa-check"></i>
+        <p class="prueba">la respuesta es correcta</p>
+        <p>la opcion elegida es ${op}</p>`
+      rta.innerHTML=texto; 
+      rta.style.setProperty('position', "relative");
+      rta.style.setProperty('display', "flex");
+      rta.style.setProperty('left', "60px");
+      rta.style.setProperty('color', "green");
+      rta.style.setProperty('width', "100%");
+    }else if(op!=op2 && op2!=op3 && op3!=op){
+        e.preventDefault();
+        let texto=`
+        <i class="fa-solid fa-x"></i>
+        <p class="prueba">la respuesta es incorrecta</p>`
+      rta.innerHTML=texto;
+      rta.style.setProperty('position', "relative");
+      rta.style.setProperty('display', "flex");
+      rta.style.setProperty('left', "60px");
+      rta.style.setProperty('color', "red");
+    }else{
+        e.preventDefault();
+        let texto=`
+        <i class="fa-solid fa-x"></i>
+        <p class="prueba">la respuesta es incorrecta</p>`
+      rta.innerHTML=texto;
+      rta.style.setProperty('position', "relative");
+      rta.style.setProperty('display', "flex");
+      rta.style.setProperty('left', "60px");
+      rta.style.setProperty('color', "red");
+    }
+})
