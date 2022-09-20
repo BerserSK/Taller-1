eb9feff2dcaeaee4192ac4c02cad5acae2f35a69
Your Name <andres2003latorre@gmail.com>
2022-09-20 12:08:35 -0500

    Haciendo la validacion de formulario

--- js/validacion.js
+++ js/validacion.js
@@ -2,12 +2,22 @@
 //Esta es para nombre y apellidos ^[a-zA-ZÁ-ÿ\s]{1,40}$
 //Esta es para el correo ^[a-zA-Z0-9_+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
 //Esta es para el password ^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{8,}$
-
+//Esta es para fechas ^(?:3[01]|[12][0-9]|0?[1-9])([\-/.])(0?[1-9]|1[1-2])\1\d{4}$
 const reglas={
     textos: /^[a-zA-ZÁ-ÿ\s]{1,40}$/, //Solo letras
     numeros: /^\d{7,10}$/, //Solo numeros
     correo: /^[a-zA-Z0-9_+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //Correos
-    password: /^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{8,}$/ //Passwords
+    password: /^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{8,}$/, //Passwords
+    fechas: /^(?:3[01]|[12][0-9]|0?[1-9])([\-/.])(0?[1-9]|1[1-2])\1\d{4}$/ //Fechas
+}
+
+const inputs={
+    numdoc: false,
+    nombre: false,
+    apellido: false,
+    email: false,
+    telefono: false,
+    password: false
 }
 
 //Acceder al formulario
@@ -27,14 +37,18 @@ const validarInput=(regla,input,grupo)=>{
         document.querySelector(`#g-${grupo} i`).classList.add('fa-check');
         document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-exclamation');
         document.querySelector(`#g-${grupo} .msn-error`).classList.remove('msn-error-visible')
+        inputs[grupo]=true
     }else{
         document.getElementById(`g-${grupo}`).classList.add('error');
         document.getElementById(`g-${grupo}`).classList.remove('success');
         document.querySelector(`#g-${grupo} i`).classList.remove('fa-check');
         document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-exclamation');
         document.querySelector(`#g-${grupo} .msn-error`).classList.add('msn-error-visible');
+        inputs[grupo]=false
     }
 }
+
+
 
 const validarCampos=(e)=>{
     console.log("Se genero un eveto sobre el input"+e.target.name)
@@ -51,10 +65,16 @@ const validarCampos=(e)=>{
         case "telefono":
             validarInput(reglas.numeros,e.target,e.target.name);
         break;
+        case "nacimiento":
+            validarInput(reglas.fechas,e.target,e.target.name);
+        break;
         case "email":
             validarInput(reglas.correo,e.target,e.target.name);
         break;
         case "password":
+            validarInput(reglas.password,e.target,e.target.name)
+        break;
+        case "password2":
             validarInput(reglas.password,e.target,e.target.name)
         break;
         default:
@@ -68,11 +88,34 @@ campos.forEach((campo)=>{
     campo.addEventListener('blur',validarCampos)
 })
 
+form.addEventListener('submit', e=>{
+    e.preventDefault();
+    const terminos = document.getElementById('terminos');
+    if(inputs.numdoc && inputs.nombre && inputs.apellido && inputs.email && inputs.telefono && inputs.password && terminos.cheked){
+        alert("El usuario ha sido registrado");
+        form.reset();
+        document.querySelectorAll('.success').forEach(icono=>{
+            icono.classList.remove('success')
+        })
+    }else{
+        document.querySelectorAll('.success').forEach(icono=>{
+            icono.classList.add('error')
+        })
+    }
+})
 
 
+    
+/*function comprobarClave(){
+    
+    clave1 = document.getElementById('password').value
+    clave2 = document.getElementById('password2').value
 
-
-
+    if (clave1 == clave2)
+       alert("Las dos claves son iguales...\nRealizaríamos las acciones del caso positivo")
+    else
+       alert("Las dos claves son distintas...\nRealizaríamos las acciones del caso negativo")
+}*/
 
 
 
@@ -96,4 +139,3 @@ window.addEventListener('click',(e)=>{
         venmodalv.style.display="none"
     }
 })
-
